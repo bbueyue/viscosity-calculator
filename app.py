e8a72e0f2f5b16e164aa7f13906c9c2947e0aab1
ralajan <raghava.alajangi@mpl.mpg.de>
2022-07-05 16:40:18 +0200

    Code enhancement

--- app.py
+++ app.py
@@ -15,124 +15,166 @@ server = app.server
 app.layout = html.Div(children=[
                   html.Div(className='row',  # Define the row element
                            children=[
-                           # Add the logo
-                           html.Img(src="assets/logo.svg",  width=400, style={'display' : 'inline-block'}),
-                           html.Div(style={'text-align':'center', },
-                                    children = [
+                           # Add max planck logo
+                           html.Img(src="assets/logo.svg",  width=300,
+                                    style={'display' : 'inline-block',
+                                           'padding-top': '20px',
+                                           'padding-left': '40px'
+                                            }),
+                           # Add reference github link
+                           html.A("Edit on GitHub",
+                                  href='https://github.com/bbueyue/viscosity'
+                                       '-calculator',
+                                  target="_blank",
+                                  style={'display' : 'inline-block',
+                                         'float' : 'right',
+                                         'font-size': '15px',
+                                         'color' : '#017b70',
+                                         'padding-top': '30px',
+                                         'padding-right': '40px'
+                                         }),
+                           html.Div(style={'text-align':'center'},
+                                    children=[
                                         # App title
-                                        html.P('RT-DC Buffers Viscosity Calculator', style = {'font-size': '25px', 'text-align':'center'}),
-                                        html.Br(),
-                                        # Dropdown menu division (titile and design)
+                                        html.P('RT-DC Buffers Viscosity '
+                                               'Calculator',
+                                               style={'font-size': '22px',
+                                                      'text-align':'center',
+                                                      'padding-bottom': '10px'
+                                                     }),
+                                        # html.Br(), Dropdown menu component
+                                        # (title and design)
                                         html.Div([
-                                              html.H2('Medium', style = {'font-size': '14px'}),
+                                              html.H2('Medium',
+                                                      style={'font-size': '14px'
+                                                             }),
                                               dcc.Dropdown(id="medium",
-                                                           options=[
-                                                                   {'label': '0.5% MC-PBS', 'value': 'M1'},
-                                                                   {'label': '0.6% MC-PBS', 'value': 'M2'},
-                                                                   {'label': '0.84% MC-PBS', 'value': 'M3'}
-                                                                    ],
                                                            searchable=True,
                                                            placeholder='Select a medium',
-                                                           style={'width': '250px', 
+                                                           options=[
+                                                           {'label': '0.5% MC-PBS', 'value': '1'},
+                                                           {'label': '0.6% MC-PBS', 'value': '2'},
+                                                           {'label': '0.84% MC-PBS', 'value': '3'}
+                                                                    ],
+                                                           style={'width': '250px',
+                                                                  'height' : '35px',
                                                                   'color': 'blue',
-                                                                  'display' : 'inline-block'}
-                                                          )]),
-                                        # Temparature input box division (titile and design)    
+                                                                  'display' : 'inline-block'
+                                                                  })
+                                                  ]),
+                                        # Temperature input box component
+                                        # (title and design)
                                         html.Div([
-                                              html.H2('Temperature [°C]', style = {'font-size': '14px'}),
+                                              html.H2('Temperature [°C]',
+                                                      style={'font-size': '14px'
+                                                            }),
                                               dcc.Input(id="temperature",
+                                                        type='text',
+                                                        persistence = False,
                                                         placeholder='Enter the temperature...',
-                                                        type='text',
-                                                        persistence = False,
-                                                        style={'width': '250px'}
-                                                       )]),
-                                        # Channel size input box division (titile and design)
+                                                        style={'width': '250px',
+                                                               'height' : '35px'
+                                                               })
+                                                 ]),
+                                        # Channel size input box component
+                                        # (title and design)
                                         html.Div([
-                                              html.H2('Channel size [μm]', style = {'font-size': '14px'}),
+                                              html.H2('Channel size [μm]',
+                                                      style={'font-size': '14px'
+                                                               }),
                                               dcc.Input(id="channel_size",
+                                                        type='text',
+                                                        persistence = False,
                                                         placeholder='Enter the channel size...',
-                                                        type='text',
-                                                        persistence = False,
-                                                        style={'width': '250px'}
-                                                       )]),
-                                        # Flowrate input box division (titile and design)    
+                                                        style={'width': '250px',
+                                                               'height' : '35px'
+                                                               })
+                                                 ]),
+                                        # Flow rate input box component
+                                        # (title and design)
                                         html.Div([
-                                              html.H2('Flowrate [μl/s]', style = {'font-size': '14px'}),
-                                              dcc.Input(id="flowrate",
-                                                        placeholder='Enter the flowrate...',
+                                              html.H2('Flowrate [μl/s]',
+                                                      style={'font-size': '14px'
+                                                               }),
+                                              dcc.Input(id="flow_rate",
                                                         type='text',
                                                         persistence = False,
-                                                        style={'width': '250px'}
-                                                       )]),
+                                                        placeholder='Enter the flow-rate...',
+                                                        style={'width': '250px',
+                                                               'height' : '35px'
+                                                               })
+                                                 ]),
                                         html.Br(), 
                                         # Store component for input storage
-                                        dcc.Store(id = 'store_viscosity'),
+                                        dcc.Store(id='store_viscosity'),
                                         # Submit button design 
-                                        html.Button('Submit', id='submit_button', n_clicks=0, disabled=False, 
-                                                    style = {'font-size': '12px',
-                                                              'cursor': 'pointer',
-                                                              'text-align': 'center',
-                                                              'color': 'white',
-                                                            }
-                                                    ),
+                                        html.Button('Submit',
+                                                    id='submit_button',
+                                                    n_clicks=0, disabled=False,
+                                                    style={'font-size': '12px',
+                                                           'cursor': 'pointer',
+                                                           'text-align': 'center',
+                                                           'color': 'white',
+                                                            }),
                                         html.Br(), html.Br(), 
-                                        # Output component to display viscosity value
-                                        html.Div(id='show_viscosity', style={'whiteSpace': 'pre-line', 
-                                                                                'font-size': '20px'})
-                                        
-                                  ]),  
+                                        # Output component to display viscosity
+                                        html.Div(id='show_viscosity',
+                                                 style={'whiteSpace': 'pre-line',
+                                                        'font-size': '20px'
+                                                        })
+                                  ]),
                           ]) # row Div
                     ]) # main Div
+
+
+# Function to compute the viscosity value
+# based on user given input
+def compute_viscosity(float_feats):
+    medium, temp, chsize, flwrate = float_feats
+    if medium == 1.0:
+        n_MC0X = 0.0026*temp + 0.590
+        k_MC0X = 0.05*np.exp(35*(1/temp))
+    if medium == 2.0:
+        n_MC0X = 0.0024*temp + 0.529
+        k_MC0X = 0.15*np.exp(27.8*(1/temp))
+    if medium == 3.0:
+        n_MC0X = 0.0021*temp + 0.467
+        k_MC0X = 0.40*np.exp(30.6*(1/temp))
+    shear_rate = 8*flwrate/((chsize*1e-3)**3)*(0.6671+0.2121/n_MC0X)
+    viscosity = k_MC0X*(shear_rate**(n_MC0X - 1))*1000
+    viscosity = round(viscosity, 3)
+    return viscosity
+
 
 # This callback function takes the inputs from
 # the html components (see the id's), compute 
 # the viscosity and store it in dcc.Store 
-# component as key and value pair
+# component as s dictionary.
 @app.callback(
     Output("store_viscosity", "data"),
    [Input("medium", "value"),
     Input("temperature", "value"),
     Input("channel_size", "value"),
-    Input("flowrate", "value")])
+    Input("flow_rate", "value")])
+def store_viscosity(medium, temperature, channel_size, flow_rate):
+    # dashboard inputs from users are of string type
+    string_feats = [medium, temperature, channel_size, flow_rate]
+    if len(string_feats) != 4 or None in string_feats or '' in string_feats:
+        return None
+    # Convert string inputs into float
+    float_feats = list(map(float, string_feats))
+    viscosity = compute_viscosity(float_feats)
+    return {'viscosity' : viscosity}
 
-def compute_viscosity(medium, temperature, channel_size, flow_rate):
-    features_str = [temperature, channel_size, flow_rate]
-    if len(features_str) == 3 and None not in features_str and '' not in features_str:
 
-        if medium=='M1':
-            n_MC05 = 0.0026*float(temperature) + 0.590
-            K_MC05 = 0.05*np.exp(35*(1/float(temperature)))
-            shear_rate_05 = 8*float(flow_rate)/((float(channel_size)*1e-3)**3)*(0.6671+0.2121/n_MC05)
-            viscosity = K_MC05*shear_rate_05**(n_MC05-1)*1000
-            viscosity = np.round(viscosity,2)
-            return {'viscosity' : viscosity}
-
-        if medium=='M2':
-            n_MC06 = 0.0024*float(temperature) + 0.529
-            K_MC06 = 0.15*np.exp(27.8*(1/float(temperature)))
-            shear_rate_06 = 8*float(flow_rate)/((float(channel_size)*1e-3)**3)*(0.6671+0.2121/n_MC06)
-            viscosity = K_MC06*shear_rate_06**(n_MC06-1)*1000#
-            viscosity = np.round(viscosity,2)
-            return {'viscosity' : viscosity}
-
-        if medium=='M3':
-            n_MC084 = 0.0021*float(temperature) + 0.467
-            K_MC084 = 0.40*np.exp(30.6*(1/float(temperature)))
-            shear_rate_084 = 8*float(flow_rate)/((float(channel_size)*1e-3)**3)*(0.6671+0.2121/n_MC084)
-            viscosity = K_MC084*shear_rate_084**(n_MC084-1)*1000
-            viscosity = np.round(viscosity, 2)
-            return {'viscosity' : viscosity}
-
-  
 # This call back function is activated when 
 # user click on submit button. When do so, stored
-# viscosity value will be diyplayed in the output 
+# viscosity value will be displayed in the output
 # component
 @app.callback(
     Output('show_viscosity', 'children'),
     Input('submit_button', 'n_clicks'),
     State('store_viscosity', 'data'))
-
 def display_output(n_clicks, stored_viscosity):
     trigger = [p['prop_id'] for p in dash.callback_context.triggered][0]
     if stored_viscosity is not None:
@@ -150,9 +192,8 @@ def display_output(n_clicks, stored_viscosity):
 @app.callback(
    [Output("temperature", "value"),
     Output("channel_size", "value"),
-    Output("flowrate", "value")], 
+    Output("flow_rate", "value")],
      Input('submit_button', 'n_clicks'))
-
 def reset_inputs(click):
     trigger = [p['prop_id'] for p in dash.callback_context.triggered][0]
     if 'submit_button' in trigger:
@@ -164,7 +205,3 @@ def reset_inputs(click):
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=False)
-
-
-
-
